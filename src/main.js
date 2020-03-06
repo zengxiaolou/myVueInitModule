e633bdfca675553ef4418de28174848e7aa8b543
zengxiaolou <44358506+zengxiaolou@users.noreply.github.com>
2020-03-06 12:27:44 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//导入全局混入
+import mixins from './mixins';
+import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui';
+import '@/assets/css/index.scss'
+// 引入自定以图标
+import '@/assets/iconfont/iconfont.css'
 
-Vue.config.productionTip = false
+
+Vue.use(ElementUI);
+Vue.config.productionTip = false;
+Vue.mixin(mixins);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
---
+++ src/router/routes.js
@@ -0,0 +1,7 @@
+import errors from "@/router/modules/errors"
+// 基础路由
+const routes = [
+    ...errors,
+
+];
+export default routes;
